1828898982b418d3015f916ff03dcec87bd27197
DV1507 <78662713+DV1507@users.noreply.github.com>
2025-03-02 23:57:03 +0530

    feat: init config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Logger.log(
+    `Server running on http://localhost:${process.env.PORT ?? 3000}`,
+    'Bootstrap',
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
